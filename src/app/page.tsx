cf9391f915dc1a912073b9aa33f2d49312587bee
Muhammed Rashid M P <alrashid4star@gmail.com>
2025-06-28 07:32:34 +0530

    chore: Add Prettier and Tailwind CSS plugin configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="container h-screen flex flex-col items-center justify-center gap-4">
+    <div className="container flex h-screen flex-col items-center justify-center gap-4">
       <h1 className="text-4xl font-bold">Shadcn UI Buttons</h1>
       <div className="flex gap-4">
         <Button>Click me</Button>
